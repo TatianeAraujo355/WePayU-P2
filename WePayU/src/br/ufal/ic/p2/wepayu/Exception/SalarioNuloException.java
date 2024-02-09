67a22c0a9897bd9b48d2c9dbdc20e2f5f232ce45
PedroHkarate <83137278+PedroHkarate@users.noreply.github.com>
2024-02-09 18:45:00 -0300

    Delete WePayU directory

--- WePayU/src/br/ufal/ic/p2/wepayu/Exception/SalarioNuloException.java
+++
@@ -1,7 +0,0 @@
-package br.ufal.ic.p2.wepayu.Exception;
-
-public class SalarioNuloException extends Exception{
-    public SalarioNuloException(){
-        super("Salario nao pode ser nulo.");
-    }
-}
